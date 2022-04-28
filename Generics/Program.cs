5e3bb9f188e5e1f4425e870287e296b4cd435b50
user <user@DESKTOP-PPTU9LK>
2022-04-28 09:31:07 +0200

    Добавьте файлы проекта.

---
+++ Generics/Program.cs
@@ -0,0 +1,18 @@
+﻿Person<int> tom = new Person<int>(546, "TOM");
+Person<string> bob = new Person<string>("123sd", "Bob");
+Console.WriteLine(tom.Id); 
+Console.WriteLine(bob.Id);
+class Person<T>
+{
+    public object Id { get;}   
+    public string Name { get;}
+
+    public Person(T id, string name)
+    {
+        Id = id;                                        //соз.конструктор
+        Name = name;
+    }
+}
+
+
+
